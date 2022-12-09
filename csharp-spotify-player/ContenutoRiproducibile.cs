080e31bdce17fe04f10444997572688098c2a567
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-09 13:42:26 +0100

    Creata l'interfaccia IContenutoRiproducibile

---
+++ csharp-spotify-player/ContenutoRiproducibile.cs
@@ -0,0 +1,7 @@
+﻿namespace csharp_spotify_player {
+    public class ContenutoRiproducibile : IContenutoRiproducibile {
+        public void Play() { }
+        public void Stop() { }
+        public void Pausa() { }
+    }
+}
--- csharp-spotify-player/IContenutoRiproducibile.cs
+++ csharp-spotify-player/IContenutoRiproducibile.cs
@@ -1,15 +1,7 @@
-﻿/*
- * Creiamo un’interfaccia IContenutoRiproducibile che definisce un contratto affinché un contenuto sia riproducibile,
- * e che quindi garantisca le seguenti funzionalità:
- *  [x] Play(): metodo che deve permettere di riprodurre un contenuto audio.
- *  [x] Pausa(): metodo che permette di mettere in pausa un contenuto
- *  [x] Stop(): metodo che permette di interrompere la riproduzione dell’audio.
- */
-
-namespace csharp_spotify_player {
+﻿namespace csharp_spotify_player {
     public interface IContenutoRiproducibile {
-        public void Play();
-        public void Pausa();
-        public void Stop();
+        void Pausa();
+        void Play();
+        void Stop();
     }
 }
